403b68744cdc899508e728e010c6e71bbed6d293
Gabe Kangas <gabek@real-ity.com>
2020-11-28 19:54:29 -0800

    Add Set that will try to infer value type

--- core/data/types.go
+++ core/data/types.go
@@ -1,5 +1,10 @@
 package data
 
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+// GetString returns the string value for key.
 func (ds *Datastore) GetString(key string) (string, error) {
 	configEntry, err := ds.Get(key)
 	if err != nil {
@@ -8,11 +13,13 @@ func (ds *Datastore) GetString(key string) (string, error) {
 	return configEntry.getString()
 }
 
+// SetString will set the string value for the desired key.
 func (ds *Datastore) SetString(key string, value string) error {
 	configEntry := ConfigEntry{key, value}
 	return ds.Save(configEntry)
 }
 
+// GetNumber returns the numeric value for key.
 func (ds *Datastore) GetNumber(key string) (float32, error) {
 	configEntry, err := ds.Get(key)
 	if err != nil {
@@ -21,11 +28,13 @@ func (ds *Datastore) GetNumber(key string) (float32, error) {
 	return configEntry.getNumber()
 }
 
+// SetNumber will set the numeric value for the desired key.
 func (ds *Datastore) SetNumber(key string, value float32) error {
 	configEntry := ConfigEntry{key, value}
 	return ds.Save(configEntry)
 }
 
+// GetBool returns the boolean value for key.
 func (ds *Datastore) GetBool(key string) (bool, error) {
 	configEntry, err := ds.Get(key)
 	if err != nil {
@@ -34,7 +43,26 @@ func (ds *Datastore) GetBool(key string) (bool, error) {
 	return configEntry.getBool()
 }
 
+// SetBool will set the boolean value for the desired key.
 func (ds *Datastore) SetBool(key string, value bool) error {
 	configEntry := ConfigEntry{key, value}
 	return ds.Save(configEntry)
+}
+
+// Set will set the value for the desired key.
+func (ds *Datastore) Set(key string, value interface{}) error {
+	switch value.(type) {
+	case int, float32:
+		return ds.SetNumber(key, value.(float32))
+
+	case string:
+		return ds.SetString(key, value.(string))
+
+	case bool:
+		return ds.SetBool(key, value.(bool))
+	}
+
+	log.Panicf("Unable to store value of type %T", value)
+
+	return nil
 }
